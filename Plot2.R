dd264d51a0be7589fbe53e9d082c655a85a5d60f
gopigithubs <32552066+gopigithubs@users.noreply.github.com>
2017-11-19 14:39:49 -0800

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,14 +1,16 @@
 myDataFile <- "./data/household_power_consumption.txt"
+#Read the Data table
 myData <- read.table(myDataFile,header = TRUE,sep=";", stringsAsFactors=FALSE, na.strings="?", dec=".")
+#Subset the Data based on the date
 mySubsetData <- subset (myData, Date %in% c("1/2/2007", "2/2/2007"))
 mySubsetData$Date <- as.Date(mySubsetData$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(mySubsetData$Date), mySubsetData$Time)
 mySubsetData$Datetime <- as.POSIXct(datetime)
-
+#Plot 
 with(mySubsetData, {
   plot(Global_active_power~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
 })
-
+#Convert into PNG
 dev.copy(png, file="plot2.png", height=480, width=600)
 dev.off()
