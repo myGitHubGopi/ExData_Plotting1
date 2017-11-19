af4098f7c26c9702eaac136bfef88a4de40b30b1
gopigithubs <32552066+gopigithubs@users.noreply.github.com>
2017-11-19 14:40:54 -0800

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,16 +1,20 @@
 myDataFile <- "./data/household_power_consumption.txt"
+#Read the Data
 myData <- read.table(myDataFile,header = TRUE,sep=";", stringsAsFactors=FALSE, na.strings="?", dec=".")
+#Subset the Data based on the date
 mySubsetData <- subset (myData, Date %in% c("1/2/2007", "2/2/2007"))
 mySubsetData$Date <- as.Date(mySubsetData$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(mySubsetData$Date), mySubsetData$Time)
 mySubsetData$Datetime <- as.POSIXct(datetime)
-
+#Plot
 with(mySubsetData, {
   plot(Sub_metering_1~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~Datetime,type = "l", col='Red')
   lines(Sub_metering_3~Datetime,type = "l", col='Blue')
 })
+#Add Legend
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5,cex=0.5, pt.cex = 1, col=c("black", "red", "blue"))
+#Convert into PNG format
 dev.copy(png, file="plot3.png", height=500, width=600)
 dev.off()
