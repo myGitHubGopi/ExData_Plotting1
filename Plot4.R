aa316302b0505554834f0ff3d152a4cabea544fb
gopigithubs <32552066+gopigithubs@users.noreply.github.com>
2017-11-19 14:42:15 -0800

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,11 +1,13 @@
 myDataFile <- "./data/household_power_consumption.txt"
 myData <- read.table(myDataFile,header = TRUE,sep=";", stringsAsFactors=FALSE, na.strings="?", dec=".")
+#Subset the Data based on the date
 mySubsetData <- subset (myData, Date %in% c("1/2/2007", "2/2/2007"))
 mySubsetData$Date <- as.Date(mySubsetData$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(mySubsetData$Date), mySubsetData$Time)
 mySubsetData$Datetime <- as.POSIXct(datetime)
-
+#Convert the area into 2,2
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+#Plot
 with(mySubsetData, {
   plot(Global_active_power~Datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
@@ -20,5 +22,6 @@ with(mySubsetData, {
   plot(Global_reactive_power~Datetime, type="l", 
        ylab="Global Rective Power (kilowatts)",xlab="")
 })
+#Convert into PNG format
 dev.copy(png, file="plot4.png", height=500, width=600)
 dev.off()
